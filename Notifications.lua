b98c8b00770bd9c4dea40746e6d3f0ef72cb2ec4
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-06-05 02:13:33 +0200

    Update Notifications.lua

--- Notifications.lua
+++ Notifications.lua
@@ -1,77 +1,116 @@
 --// Services
 
 local TweenService = game:GetService("TweenService")
-local HttpService = game:GetService("HttpService")
-
---// Library Init
-
-local NotificationLibrary = {}
 
 --// Instances
 
-local Overlay = Instance.new("ScreenGui", game.CoreGui) Overlay.Name = HttpService:GenerateGUID()
-local NotificationList = Instance.new("Frame")
-local UIListLayout = Instance.new("UIListLayout")
-local UIPadding = Instance.new("UIPadding")
+local Notifications = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
+local List = Instance.new("Frame", Notifications)
+local UIListLayout = Instance.new("UIListLayout", List)
+local UIPadding = Instance.new("UIPadding", List)
 
-function NotificationLibrary:Notify(String, Time, TweenTime)
-	
-	--// Create Notification
-	
-	local Notification = Instance.new("TextLabel", NotificationList)
-	
-	Notification.BackgroundColor3 = Color3.new(1, 1, 1)
-	Notification.BackgroundTransparency = 1
-	Notification.TextTransparency = 1
-	Notification.BorderColor3 = Color3.new(0, 0, 0)
-	Notification.BorderSizePixel = 0
-	Notification.Position = UDim2.new(0.379972607, 0, 0.966648877, 0)
-	Notification.Size = UDim2.new(0.922496259, 0, 0.0333511084, 0)
-	Notification.Font = Enum.Font.RobotoMono
-	Notification.TextColor3 = Color3.new(1, 1, 1)
-	Notification.TextStrokeTransparency = 0.5
-	Notification.TextSize = 14
-	Notification.TextWrapped = true
-	
-	local Entrace = TweenService:Create( Notification, TweenInfo.new(tonumber(TweenTime)), { TextTransparency = 0 } )
-	local Exit = TweenService:Create( Notification, TweenInfo.new(tonumber(TweenTime)), { TextTransparency = 1 } )
-	
-	Entrace:Play()
-	
-	Notification.Text = tostring(String)
-	
-	repeat
-		task.wait()
-	until Entrace.Completed
-	
-	task.wait(Time)
-	
-	Exit:Play()
-	
-	repeat
-		task.wait()
-	until Exit.Completed
-	
-	Notification:Destroy()
+--// Tables
 
+local Library = {}
+
+--// Rest
+
+do --// Logic
+	
+	do --// UI
+		
+		do --// Properties
+			
+			do --// List
+				
+				List.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+				List.BackgroundTransparency = 1.000
+				List.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				List.BorderSizePixel = 0
+				List.Position = UDim2.new(0.421, 0,-0, 0)
+				List.Size = UDim2.new(0.157169074, 0, 1.00000012, 0)
+				
+			end
+
+			do --// UIListLayout
+				
+				UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+				UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+				UIListLayout.Padding = UDim.new(0, 5)
+				
+			end
+
+			do --// Padding
+				
+				UIPadding.PaddingTop = UDim.new(0, 5)
+				
+			end
+			
+		end
+		
+	end
+	
+	do --// Functions
+		
+		function Library:Notify(Str, Time, Tween, Origin)
+			
+			local Notification = Instance.new("TextLabel", List)
+			UIListLayout.VerticalAlignment = Enum.VerticalAlignment[Origin]
+			
+			do --// Properties
+				
+				
+				Notification.Parent = List
+				Notification.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+				Notification.BackgroundTransparency = 1.000
+				Notification.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				Notification.BorderSizePixel = 0
+				Notification.Position = UDim2.new(0.0207064692, 0, -1.15877785e-09, 0)
+				Notification.Size = UDim2.new(0.958586633, 0, 0.0360851437, 0)
+				Notification.Font = Enum.Font.RobotoMono
+				Notification.TextColor3 = Color3.fromRGB(255, 255, 255)
+				Notification.TextSize = 14.000
+				Notification.TextStrokeTransparency = 0.000
+				Notification.TextWrapped = true
+				Notification.TextTransparency = 1
+				Notification.Text = tostring(Str)
+				
+				do --// Tweening
+					
+					local FadeIn = TweenService:Create( Notification, TweenInfo.new(Tween), { TextTransparency = 0 } )
+					local FadeOut = TweenService:Create( Notification, TweenInfo.new(Tween), { TextTransparency = 0 } )
+					
+					do --// Logic
+						
+						FadeIn:Play()
+						
+						do --// Connections
+
+							FadeIn.Completed:Connect(function()
+
+								task.wait(Time)
+								FadeOut:Play()
+
+							end)
+
+							FadeOut.Completed:Connect(function()
+
+								Notification:Destroy()
+
+							end)
+
+						end
+						
+					end
+					
+				end
+				
+			end
+			
+		end
+		
+	end
+	
 end
 
-NotificationList.Name = "NotificationList"
-NotificationList.Parent = Overlay
-NotificationList.BackgroundColor3 = Color3.new(1, 1, 1)
-NotificationList.BackgroundTransparency = 1
-NotificationList.BorderColor3 = Color3.new(0, 0, 0)
-NotificationList.BorderSizePixel = 0
-NotificationList.Position = UDim2.new(0.397615701, 0, 0.0311720707, 0)
-NotificationList.Size = UDim2.new(0.204488143, 0, 0.873067319, 0)
-
-UIListLayout.Parent = NotificationList
-UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
-UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-
-UIPadding.Parent = NotificationList
-UIPadding.PaddingBottom = UDim.new(0, 10)
-UIPadding.PaddingTop = UDim.new(0, 10)
-
-return NotificationLibrary
+return Library
