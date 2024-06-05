e94a90ea4c45d72aa6559de238520d7c165e7db4
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-06-05 02:17:10 +0200

    Update Notifications.lua

--- Notifications.lua
+++ Notifications.lua
@@ -16,50 +16,50 @@ local Library = {}
 --// Rest
 
 do --// Logic
-	
+
 	do --// UI
-		
+
 		do --// Properties
-			
+
 			do --// List
-				
+
 				List.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 				List.BackgroundTransparency = 1.000
 				List.BorderColor3 = Color3.fromRGB(0, 0, 0)
 				List.BorderSizePixel = 0
 				List.Position = UDim2.new(0.421, 0,-0, 0)
 				List.Size = UDim2.new(0.157169074, 0, 1.00000012, 0)
-				
+
 			end
 
 			do --// UIListLayout
-				
+
 				UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 				UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
 				UIListLayout.Padding = UDim.new(0, 5)
-				
+
 			end
 
 			do --// Padding
-				
+
 				UIPadding.PaddingTop = UDim.new(0, 5)
-				
+
 			end
-			
+
 		end
-		
+
 	end
-	
+
 	do --// Functions
-		
+
 		function Library:Notify(Str, Time, Tween, Origin)
-			
+
 			local Notification = Instance.new("TextLabel", List)
 			UIListLayout.VerticalAlignment = Enum.VerticalAlignment[Origin]
-			
+
 			do --// Properties
-				
-				
+
+
 				Notification.Parent = List
 				Notification.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 				Notification.BackgroundTransparency = 1.000
@@ -74,16 +74,16 @@ do --// Logic
 				Notification.TextWrapped = true
 				Notification.TextTransparency = 1
 				Notification.Text = tostring(Str)
-				
+
 				do --// Tweening
-					
+
 					local FadeIn = TweenService:Create( Notification, TweenInfo.new(Tween), { TextTransparency = 0 } )
 					local FadeOut = TweenService:Create( Notification, TweenInfo.new(Tween), { TextTransparency = 1 } )
-					
+
 					do --// Logic
-						
+
 						FadeIn:Play()
-						
+
 						do --// Connections
 
 							FadeIn.Completed:Connect(function()
@@ -100,17 +100,17 @@ do --// Logic
 							end)
 
 						end
-						
+
 					end
-					
+
 				end
-				
+
 			end
-			
+
 		end
-		
+
 	end
-	
+
 end
 
 return Library
